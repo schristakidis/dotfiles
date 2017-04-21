ef0cbed7e369ad1b9301a24b77d76f038567f1be
Sakis Christakidis <athanasios.christakidis@motionpicturesolutions.com>
2017-04-21 12:00:51 +0100

    pyenv

--- packages/pyenv.sh
+++ packages/pyenv.sh
@@ -1,6 +1,9 @@
-pyenv install 2.7.9
-pyenv rehash
-pyenv global 2.7.9
-pip install virtualenvwrapper
+sudo apt-get install libbz2-dev, libsqlite3-dev
+curl -L https://raw.githubusercontent.com/pyenv/pyenv-installer/master/bin/pyenv-installer | bash
+pyenv update
+# pyenv install 2.7.9
+# pyenv rehash
+# pyenv global 2.7.9
+sudo pip install virtualenvwrapper
 pyenv global system
-
+git clone https://github.com/yyuu/pyenv-virtualenvwrapper.git ~/.pyenv/plugins/pyenv-virtualenvwrapper
