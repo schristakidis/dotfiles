2bed006388b0fadce95709206835b270558f44cf
Sakis Christakidis <athanasios.christakidis@motionpicturesolutions.com>
2017-10-21 21:37:31 +0100

    custom powerline segment for ale integration

---
+++ powerline/segments/custom/ale.py
@@ -0,0 +1,40 @@
+# vim:fileencoding=utf-8:noet
+from __future__ import (
+    unicode_literals, division, absolute_import, print_function
+)
+
+try:
+    import vim
+except ImportError:
+    vim = object()
+
+from powerline.segments.vim import window_cached
+
+
+@window_cached
+def ale(pl, err_format='{num} ERR', warn_format='{num} WARN'):
+    '''Show whether ale has found any errors or warnings
+
+    :param str err_format:
+                    Format string for errors.abs
+    :param str warn_format:
+                    Format string for warnings.abs
+    Highlight groups used: ``ale:warning`` or ``warning``, ``ale:error`` or ``error``.abs
+    '''
+
+    res = vim.eval('AleLinterStatus()')
+
+    if isinstance(res, list):
+        (errors, warnings) = res
+        segments = []
+        if errors > 0:
+            segments.append({
+                'contents': err_format.format(num=errors),
+                'highlight_groups': ['ale:error', 'error'],
+            })
+        if warnings > 0:
+            segments.append({
+                'contents': warn_format.format(num=warnings),
+                'highlight_groups': ['ale:warning', 'warning'],
+                })
+        return segments
