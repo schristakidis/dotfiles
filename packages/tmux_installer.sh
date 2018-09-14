a56806559e5dfb413d2f675f9adbd32c2d6d500b
Sakis Christakidis <athanasios.christakidis@motionpicturesolutions.com>
2018-09-14 15:58:49 +0100

    no more vim-tmux-clipboard

--- packages/tmux_installer.sh
+++ packages/tmux_installer.sh
@@ -1,7 +1,7 @@
 cd /tmp
 apt-get install libevent-dev ncurses-dev
-wget https://github.com/tmux/tmux/releases/download/2.2/tmux-2.2.tar.gz
-tar -zxf tmux-2.2.tar.gz
-cd tmux-2.2
+wget https://github.com/tmux/tmux/releases/download/2.6/tmux-2.6.tar.gz
+tar -zxf tmux-2.6.tar.gz
+cd tmux-2.6
 ./configure && make
 cp tmux /usr/local/bin/tmux
