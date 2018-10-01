c8def6bd5a32ccf8ab49176ad0329ce3291cc92c
Sakis Christakidis <athanasios.christakidis@motionpicturesolutions.com>
2018-10-01 23:27:54 +0100

    better coloring of the tab completion menu

--- zsh/prezto.zsh
+++ zsh/prezto.zsh
@@ -5,3 +5,5 @@ zstyle ':prezto:*:*' color 'yes'
 
 zstyle ':prezto:module:prompt' theme 'pure'
 
+zstyle ':completion:*' menu select
+zstyle -e ':completion:*:default' list-colors 'reply=("${PREFIX:+=(#bi)($PREFIX:t)(?)*==34=34}:${(s.:.)LS_COLORS}")'
