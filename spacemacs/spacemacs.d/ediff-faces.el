53085fb4b8681b99a85a3431a16fd9cbf87b5b37
Sakis Christakidis <athanasios.christakidis@motionpicturesolutions.com>
2020-06-20 19:24:11 +0100

    bye bye spacemacs/zplug

--- spacemacs/spacemacs.d/ediff-faces.el
+++
@@ -1,8 +0,0 @@
-(with-eval-after-load 'ediff
-  (set-face-attribute ediff-even-diff-face-A nil :background "dark slate grey")
-  (set-face-attribute ediff-even-diff-face-B nil :background "dark slate gray")
-  (set-face-attribute ediff-fine-diff-face-B nil :background "saddle brown")
-  (set-face-attribute ediff-odd-diff-face-A nil :background "gray15")
-  (set-face-attribute ediff-odd-diff-face-B nil :background "gray15"))
-
-(provide 'ediff-faces)
