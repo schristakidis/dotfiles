02a3cf0fc4183d897a5d23ff224c0a34d98f7979
Sakis Christakidis <athanasios.christakidis@motionpicturesolutions.com>
2021-12-20 11:41:21 +0000

    install openconnect-sso to a 3.9 virtualenv

--- bin/mps_connect.sh
+++ bin/mps_connect.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 SLEEP_TIME=60 aws_connect.sh &
-openconnect-sso --server vpn.motionpicturesolutions.com --user athanasios.christakidis@motionpicturesolutions.com
+source $HOME/.virtualenvs/openconnect/bin/activate && openconnect-sso --server vpn.motionpicturesolutions.com --user athanasios.christakidis@motionpicturesolutions.com
