ca01c26f02c5ab12fd734372349e9dca9f84415b
Sakis Christakidis <athanasios.christakidis@motionpicturesolutions.com>
2022-03-01 12:09:42 +0000

    changed login cluster

--- bin/aws_connect.sh
+++ bin/aws_connect.sh
@@ -9,7 +9,7 @@ fi
 aws-azure-login --profile=production --no-prompt
 aws-azure-login --profile=staging --no-prompt
 
-aws eks --region eu-west-1 update-kubeconfig --name staging-01 --profile staging
+aws eks --region eu-west-1 update-kubeconfig --name staging-02 --profile staging
 
 systemctl --user start aws.timer
 systemctl --user start aws_prod.timer
