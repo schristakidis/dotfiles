ccd05199bca3cc1502c59c5a0acea4f6a517a11a
Sakis Christakidis <athanasios.christakidis@motionpicturesolutions.com>
2022-03-27 13:32:51 +0100

    moving to alacritty

---
+++ http_url_handler/install-handler.sh
@@ -0,0 +1,31 @@
+#! /usr/bin/env sh
+MIMETYPE1="x-scheme-handler/http"
+MIMETYPE2="x-scheme-handler/https"
+DESKTOP_FILEPATH=http-url-handler.desktop
+DESKTOP_FILENAME=$(basename "${DESKTOP_FILEPATH}")
+
+BIN_FILE="$HOME/.local/bin/http_url_handler"
+
+apps="${XDG_DATA_HOME:-$HOME/.local/share}/applications"
+
+if ! test -d "${apps}"
+then
+    mkdir -p "${apps}"
+fi
+
+desktop-file-install --dir="${apps}" "${DESKTOP_FILEPATH}"
+
+xdg-mime default "${DESKTOP_FILENAME}" "${MIMETYPE1}"
+xdg-mime default "${DESKTOP_FILENAME}" "${MIMETYPE2}"
+
+if command gio 2> /dev/null
+then
+    gio mime "${MIMETYPE1}" "${DESKTOP_FILENAME}"
+    gio mime "${MIMETYPE2}" "${DESKTOP_FILENAME}"
+fi
+# For applications that read mimeinfo.cache instead of mimeapps.list:
+update-desktop-database "${apps}"
+
+if [ ! -f $BIN_FILE ]; then
+    ln -s $PWD/http_url_handler $BIN_FILE
+fi
