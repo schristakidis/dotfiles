b444a85abce01dccd7dd7e96d50c79668c0f4750
Sakis Christakidis <athanasios.christakidis@motionpicturesolutions.com>
2022-06-01 00:00:42 +0100

    small update

--- bin/mps_connect.sh
+++ bin/mps_connect.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 export QTWEBENGINE_CHROMIUM_FLAGS="--no-sandbox"
-SLEEP_TIME=60 aws_connect.sh &
+# SLEEP_TIME=60 aws_connect.sh &
 source $HOME/.virtualenvs/openconnect/bin/activate && openconnect-sso --server vpn.motionpicturesolutions.com --user athanasios.christakidis@motionpicturesolutions.com
