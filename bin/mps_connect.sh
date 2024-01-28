3bb6ed5c54fb626152e4d4c6a8981c74d69759c7
Sakis Christakidis <athanasios.christakidis@motionpicturesolutions.com>
2024-01-28 15:03:09 +0000

    fixes

--- bin/mps_connect.sh
+++ bin/mps_connect.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 export QTWEBENGINE_CHROMIUM_FLAGS="--no-sandbox"
-export OPENSSL_CONF=./openssl.cnf
+export OPENSSL_CONF=~/.dotfiles/bin/openssl.cnf
 source $HOME/.pyenv/versions/opensso/bin/activate && openconnect-sso --server vpn.motionpicturesolutions.com --user athanasios.christakidis@motionpicturesolutions.com
--- bin/thes_mps_connect.sh
+++ bin/thes_mps_connect.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 export QTWEBENGINE_CHROMIUM_FLAGS="--no-sandbox"
-export OPENSSL_CONF=./openssl.cnf
+export OPENSSL_CONF=~/.dotfiles/bin/openssl.cnf
 source $HOME/.pyenv/versions/opensso/bin/activate && openconnect-sso --server vpn.motionpicturesolutions.com --user athanasios.christakidis@motionpicturesolutions.com -- --base-mtu=1450
 
