4489eeeb6f6fb0a8be5871d4efa883b1ca704335
Sakis Christakidis <athanasios.christakidis@motionpicturesolutions.com>
2024-10-27 16:17:35 +0000

    going back to swayidle. Better hyprlock config

--- .config/hypr/lock.sh
+++ .config/hypr/lock.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+cd /tmp
+grim screen.png
+
+# Delete existing image
+rm screen-out.png
+
+#Adds a blur and vignette
+ffmpeg -i screen.png -vf "gblur=sigma=10, vignette=PI/5" -c:a copy screen-out.png
+
 1password --lock
 pidof hyprlock || hyprlock
 
