4e3472e343c86fbaf3a4e7cb1001bd3e12582a4c
Sakis Christakidis <athanasios.christakidis@motionpicturesolutions.com>
2025-07-24 14:36:57 +0100

    lsp info/status commands

--- .config/nvim/lua/core/plugins/treesiter.lua
+++ .config/nvim/lua/core/plugins/treesiter.lua
@@ -13,7 +13,7 @@ local M = {
         require("nvim-treesitter.configs").setup({
             ensure_installed = {
                 "c", "lua", "vim", "vimdoc", "query", "python", "go", "terraform", "yaml",
-                "dockerfile", "json", "markdown", "norg", "markdown_inline"
+                "dockerfile", "json", "markdown", "norg", "markdown_inline", "gotmpl"
             },
             ignore_install = { "phpdoc" }, -- List of parsers to ignore installing
             sync_install = false,
