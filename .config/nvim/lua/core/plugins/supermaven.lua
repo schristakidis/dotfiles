18f6d095fe8964cbc1c291fa89c3f80f89aebe12
Sakis Christakidis <athanasios.christakidis@motionpicturesolutions.com>
2026-01-14 14:30:24 +0000

    disabling supermaven for some filetypes

--- .config/nvim/lua/core/plugins/supermaven.lua
+++ .config/nvim/lua/core/plugins/supermaven.lua
@@ -2,7 +2,8 @@ M = {
 	"supermaven-inc/supermaven-nvim",
 	config = function()
 		require("supermaven-nvim").setup({
-			disable_inline_completion = true
+			disable_inline_completion = true,
+			ignore_filetypes = { "markdown", "norg", "json", "text", "log" }
 		})
 	end,
 }
