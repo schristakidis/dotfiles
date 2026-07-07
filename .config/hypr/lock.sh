5e68fcf8427141409e3c1722a3babca0b9a45532
Sakis Christakidis <athanasios.christakidis@motionpicturesolutions.com>
2026-07-07 10:11:29 +0100

    move to hyperidle

--- .config/hypr/lock.sh
+++ .config/hypr/lock.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-cd /tmp
-grim screen.png
+# cd /tmp
+# grim screen.png
 
 # Delete existing image
-rm screen-out.png
+# rm screen-out.png
 
 #Adds a blur and vignette
-ffmpeg -i screen.png -vf "gblur=sigma=10, vignette=PI/5" -c:a copy screen-out.png
+# ffmpeg -i screen.png -vf "gblur=sigma=10, vignette=PI/5" -c:a copy screen-out.png
 
 1password --lock
 pidof hyprlock || hyprlock
